ab15c9dcec105c347fda51884138d923e75d3b2d
ttt-net <tenten102matsuyama@gmail.com>
2022-07-16 17:45:41 +0900

    Initial Commit

---
+++ IdCard-SwiftUI/IdCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdCard_SwiftUIApp.swift
+//  IdCard-SwiftUI
+//
+//  Created by tetsuta matsuyama on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct IdCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
