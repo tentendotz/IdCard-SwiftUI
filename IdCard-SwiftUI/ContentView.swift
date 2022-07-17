42bd8591742021fdcc8c196c590cdbbc8d4ed8b7
ttt-net <tenten102matsuyama@gmail.com>
2022-07-17 20:22:03 +0900

    Add rounded image for layout

--- IdCard-SwiftUI/ContentView.swift
+++ IdCard-SwiftUI/ContentView.swift
@@ -13,10 +13,27 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.64, green: 0.61, blue: 1.00, opacity: 1.00)
                 .edgesIgnoringSafeArea(.all)
-            Text ("tentenblog")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .foregroundColor(.white)
-                .bold()
+            
+            VStack {
+                Image("tentenblog")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(alignment: .center) {
+                        Circle()
+                            .stroke(Color.white, lineWidth: 5)
+                    }
+                Text("tentenblog")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                    .bold()
+                Text("iOS Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                
+                Divider()
+            }
         }
     }
 }
